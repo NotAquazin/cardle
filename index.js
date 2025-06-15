735f5660803e74cabb3be55a8221a0f847a43a6c
Wahab-a-web <abdul.iburahim@student.ateneo.edu>
2025-06-16 00:30:08 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+//console.log(`Hello`);
+//console.log("Mother");
+
+//window.alert(`This is an alert!`);
+//window.alert(`Porsche`);
+
+//document.getElementById("myH1").textContent = `Hello`;
+//document.getElementById("myP").textContent = `I like Porsche`;
+
+
+// comment
+/* java like comments 
+   a;lksdjf;alskd
+   a;dlskfja;klfj
+*/
+
+/*
+let username;
+
+document.getElementById("mySubmit").onclick = function(){
+   username = document.getElementById("myText").value;
+   document.getElementById("myH1").textContent = `Hello ${username}`;
+}
+*/
+
+let x = "pizza";
+let y = true;
+let z = true;
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x); //Nan (Not a Number) 'number'
+console.log(y, typeof y); //pizza string
+console.log(z, typeof z); //true 'boolean
